900465e26369e5b85ffd91e71d12d2ae61baf5c4
Yuki Nagae <yuki.nagae1130@gmail.com>
2019-09-03 21:42:58 +0900

    Tod CRUD on dummy data

--- design/design.go
+++ design/design.go
@@ -16,19 +16,66 @@ var _ = API("todo", func() {
 })
 
 var _ = Service("todo", func() {
+
+	HTTP(func() {
+		Path("/")
+	})
+
 	Method("get", func() {
 		Payload(func() {
 			Field(1, "id", String, "Todo ID")
 			Required("id")
 		})
-
 		Result(Todo)
-
+		Error("not_found", NotFound, "Bottle not found")
 		HTTP(func() {
 			GET("/{id}")
+			Response(StatusOK)
 		})
-
 		GRPC(func() {
+			Response(CodeOK)
+		})
+	})
+
+	Method("list", func() {
+		Result(CollectionOf(Todo))
+		HTTP(func() {
+			GET("/")
+			Response(StatusOK)
+		})
+		GRPC(func() {
+			Response(CodeOK)
+		})
+	})
+
+	Method("add", func() {
+		Payload(func() {
+			Field(1, "id", String, "Todo ID")
+			Field(2, "task", String, "Todo task")
+			Required("id", "task")
+		})
+		Result(String)
+		HTTP(func() {
+			POST("/")
+			Response(StatusCreated)
+		})
+		GRPC(func() {
+			Response(CodeOK)
+		})
+	})
+
+	Method("remove", func() {
+		Payload(func() {
+			Field(1, "id", String, "Todo ID")
+			Required("id")
+		})
+		Error("not_found", NotFound, "Bottle not found")
+		HTTP(func() {
+			DELETE("/{id}")
+			Response(StatusNoContent)
+		})
+		GRPC(func() {
+			Response(CodeOK)
 		})
 	})
 
@@ -46,4 +93,16 @@ var Todo = ResultType("Todo", func() {
 		Meta("rpc:tag", "2")
 	})
 	Required("id", "task")
+})
+
+// NotFound is Type
+var NotFound = Type("NotFound", func() {
+	Description("NotFound is the type returned when attempting to show or delete a todo that does not exist.")
+	Attribute("message", String, "Message of error", func() {
+		Meta("struct:error:name")
+		Example("todo todo1 not found")
+		Meta("rpc:tag", "1")
+	})
+	Field(2, "id", String, "ID of missing Todo")
+	Required("message", "id")
 })
--- todo.go
+++ todo.go
@@ -2,6 +2,7 @@ package todoapi
 
 import (
 	"context"
+	"fmt"
 	"log"
 	todo "todo/gen/todo"
 )
@@ -21,5 +22,45 @@ func NewTodo(logger *log.Logger) todo.Service {
 func (s *todosrvc) Get(ctx context.Context, p *todo.GetPayload) (res *todo.Todo, err error) {
 	res = &todo.Todo{}
 	s.logger.Print("todo.get")
-	return &todo.Todo{ID: "todo1", Task: "build an API"}, nil
+
+	for _, todo := range Todos {
+		if todo.ID == p.ID {
+			return todo, nil
+		}
+	}
+	return nil, fmt.Errorf("Todo not found: %s", p.ID)
+}
+
+// List implements list.
+func (s *todosrvc) List(ctx context.Context) (res todo.TodoCollection, err error) {
+	s.logger.Print("todo.list")
+	return Todos, nil
+}
+
+// Add implements add.
+func (s *todosrvc) Add(ctx context.Context, p *todo.AddPayload) (res string, err error) {
+	s.logger.Print("todo.add")
+	Todos = append(Todos, &todo.Todo{ID: p.ID, Task: p.Task})
+	return p.ID, nil
+}
+
+// Remove implements remove.
+func (s *todosrvc) Remove(ctx context.Context, p *todo.RemovePayload) (err error) {
+	s.logger.Print("todo.remove")
+	for i, todo := range Todos {
+		if todo.ID == p.ID {
+			Todos[i] = Todos[len(Todos)-1] // Copy last element to index i.
+			Todos[len(Todos)-1] = nil      // Erase last element (write zero value).
+			Todos = Todos[:len(Todos)-1]   // Truncate slice.
+			return nil
+		}
+	}
+	return fmt.Errorf("Todo not found: %s", p.ID)
+}
+
+// Todos is dummy data
+var Todos = []*todo.Todo{
+	{ID: "todo1", Task: "build an API"},
+	{ID: "todo2", Task: "?????"},
+	{ID: "todo3", Task: "profit!"},
 }
